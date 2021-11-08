af623967e7948bae0d2e534c0ec1f7eb983d328a
alberto garcia <albgarjim1@gmail.com>
2021-11-09 00:13:03 +0100

    feat: updated code

--- repo_push_docker.sh
+++ repo_push_docker.sh
@@ -6,5 +6,5 @@ fi
 echo "Building go app"
 CGO_ENABLED=0 go build
 
-sudo docker build -t registry.gitlab.com/aikone_fashion/lol-api:$1 .
-sudo docker push registry.gitlab.com/aikone_fashion/lol-api:$1
+sudo docker build -t registry.gitlab.com/lol-service/lol-api:$1 .
+sudo docker push registry.gitlab.com/lol-service/lol-api:$1
